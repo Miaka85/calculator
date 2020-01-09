6c5219c326fd243765311d1389b244e836bc858c
Mia <mia.karlsson@hotmail.se>
2020-01-09 11:22:54 +0100

    Tester Multiplikation och Division

--- src/MainTest.java
+++ src/MainTest.java
@@ -10,13 +10,15 @@ class MainTest {
         Double testResult = Main.addition(A, B);
         assertEquals(55, testResult);
     }
+
     @Test
-    void addition_additionBadValueTest() {
-        double C = 33;
+    void addition_additionUnexpectedValueTest() {
+        double A = 33;
         double B = 22;
-        Double testResult = Main.addition(C, B);
+        Double testResult = Main.addition(A, B);
         assertNotEquals(99, testResult);
         }
+
     @Test
     void addition_additionNegativeValueTest() {
         double A = -33;
@@ -24,17 +26,6 @@ class MainTest {
         Double testResult = Main.addition(A, B);
         assertEquals(11, testResult);
     }
-
-      /*  CustomerRegistration customerRegistration = new CustomerRegistration();
-        int start = customerRegistration.customers.size();
-        customerRegistration.customers.add(new Customer("Joel", "Ljungskile"));
-        assertEquals(start + 1, customerRegistration.customers.size());*/
-
-
-       /* String testInput = "mia";              //Tests that the first letter changes to upperCase
-        String testResult = Main.firstLetterToUpperCase(testInput);
-        assertEquals("Mia", testResult);*/
-
 
     @Test
         void main() {
@@ -48,11 +39,73 @@ class MainTest {
             assertEquals(11, testResult);
     }
 
-    @org.junit.jupiter.api.Test
-    void multiplikation() {
+    @Test
+    void subtraktion_subtraktionUnexpectedValueTest() {
+        double A = 33;
+        double B = 22;
+        Double testResult = Main.subtraktion(A, B);
+        assertNotEquals(99, testResult);
+
+    }
+
+    @Test
+    void subtraktion_subtraktionNegativeValueTest() {
+        double A = -33;
+        double B = 44;
+        Double testResult = Main.subtraktion(A, B);
+        assertEquals(-77, testResult);
+        }
+
+
+    @Test
+    void multiplikaton_multipikationTest() {
+        double A = 33;
+        double B = 22;
+        Double testResult = Main.multiplikation(A, B);
+        assertEquals(726, testResult);
+    }
+
+    @Test
+    void multiplikation_multiplikationUnexpectedValueTest() {
+        double A = 33;
+        double B = 22;
+        Double testResult = Main.multiplikation(A, B);
+        assertNotEquals(99, testResult);
+    }
+
+    @Test
+    void multiplikation_multiplikationNegativeValueTest() {
+        double A = -33;
+        double B = 44;
+        Double testResult = Main.multiplikation(A, B);
+        assertEquals(-1452, testResult);
     }
 
     @org.junit.jupiter.api.Test
     void division() {
+    }
+
+    @Test
+    void division_divisionTest() {
+        double A = 33;
+        double B = 22;
+        Double testResult = Main.division(A, B);
+        assertEquals(1.5, testResult);
+    }
+
+    @Test
+    void division_divisionUnexpectedValueTest() {
+        double A = 33;
+        double B = 22;
+        Double testResult = Main.division(A, B);
+        assertNotEquals(99, testResult);
+    }
+
+    @Test
+    void division_divisionNegativeValueTest() {
+        double A = -33;
+        double B = 44;
+        Double testResult = Main.division(A, B);
+        assertEquals(-0.75, testResult);
     }
 }
